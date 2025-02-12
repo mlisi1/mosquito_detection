e0190291832cc5c2bab25cda22999d04254dce0b
m.lisi1 <m.lisi1@studenti.unipi.it>
2025-02-12 16:51:27 +0100

    updated Dockerfile; added tools to the framework

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 xhost +
-sudo docker run --gpus all -v ~/CIB/mosquito_detection/trainings/:/workspace/trainings -v /tmp/.X11-unix/:/tmp/.X11-unix:rw -it mosquito_detection
+sudo docker run --gpus all -v ~/CIB/mosquito_detection/trainings/:/workspace/trainings -v /tmp/.X11-unix/:/tmp/.X11-unix:rw --ipc=host -it mosquito_detection
